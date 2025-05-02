313acb0a3bd1fbe724a762494d68a69a15cbfd87
Kent Harjel S. Alibango <145254312+kentharjel@users.noreply.github.com>
2025-05-02 04:03:42 -0700

    Add files via upload

---
+++ components/guest.php
@@ -0,0 +1,33 @@
+<div class="guest" id="guest">
+    <div class="guest-title">
+        <h1>GUEST</h1>
+    </div>
+
+    <br>
+
+    <div class="guest-content">
+        <div class="content">
+            <img src="../Pictures/jay.jpg" alt="">
+            <h3>Adrian Jay</h3>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 509 507.14"><path fill-rule="nonzero" d="M509 254.5C509 113.94 395.06 0 254.5 0S0 113.94 0 254.5C0 373.86 82.17 474 193.02 501.51V332.27h-52.48V254.5h52.48v-33.51c0-86.63 39.2-126.78 124.24-126.78 16.13 0 43.95 3.17 55.33 6.33v70.5c-6.01-.63-16.44-.95-29.4-.95-41.73 0-57.86 15.81-57.86 56.91v27.5h83.13l-14.28 77.77h-68.85v174.87C411.35 491.92 509 384.62 509 254.5z"/></svg>
+            </div>
+        </div>
+
+        <div class="content">
+            <img src="../Pictures//tommy.jpg" alt="">
+            <h3>Tommy Sumonod</h3>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 509 507.14"><path fill-rule="nonzero" d="M509 254.5C509 113.94 395.06 0 254.5 0S0 113.94 0 254.5C0 373.86 82.17 474 193.02 501.51V332.27h-52.48V254.5h52.48v-33.51c0-86.63 39.2-126.78 124.24-126.78 16.13 0 43.95 3.17 55.33 6.33v70.5c-6.01-.63-16.44-.95-29.4-.95-41.73 0-57.86 15.81-57.86 56.91v27.5h83.13l-14.28 77.77h-68.85v174.87C411.35 491.92 509 384.62 509 254.5z"/></svg>
+            </div>
+        </div>
+
+        <div class="content">
+            <img src="../Pictures/harjel.jpg" alt="">
+            <h3>Kent Harjel Alibango</h3>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 509 507.14"><path fill-rule="nonzero" d="M509 254.5C509 113.94 395.06 0 254.5 0S0 113.94 0 254.5C0 373.86 82.17 474 193.02 501.51V332.27h-52.48V254.5h52.48v-33.51c0-86.63 39.2-126.78 124.24-126.78 16.13 0 43.95 3.17 55.33 6.33v70.5c-6.01-.63-16.44-.95-29.4-.95-41.73 0-57.86 15.81-57.86 56.91v27.5h83.13l-14.28 77.77h-68.85v174.87C411.35 491.92 509 384.62 509 254.5z"/></svg>
+            </div>
+        </div>
+    </div>
+</div>
