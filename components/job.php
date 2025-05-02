cfaed7c60be3f16cf50b0f8bf48d64e52231d55f
Kent Harjel S. Alibango <145254312+kentharjel@users.noreply.github.com>
2025-05-02 04:02:52 -0700

    Add files via upload

---
+++ components/job.php
@@ -0,0 +1,133 @@
+<?php 
+    include '../Main/header.php';
+?>
+
+<div class="job">
+    <a class="exit" href="../index.php"><svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#000000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a>
+    <h1>JOB HIRING</h1>
+    <div class="job-hiring">
+
+        <div class="job-cover">
+            <div class="job-info">
+                <div class="job-profile">
+                    <h2>Oil Painter</h2>
+                    <img src="../Pictures/jay.jpg" alt="">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+            <div class="salary">
+                <div>
+                    <p>25O$/hr</p>
+                    <p>Address</p>
+                </div>
+                <button>DETAILS</button>
+            </div>
+        </div>
+
+        <div class="job-cover2">
+            <div class="job-info">
+                <div class="job-profile">
+                    <h2>Crayon Artist</h2>
+                    <img src="../Pictures/tommy.jpg" alt="">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+            <div class="salary">
+                <div>
+                    <p>25O$/hr</p>
+                    <p>Address</p>
+                </div>
+                <button>DETAILS</button>
+            </div>
+        </div>
+
+        <div class="job-cover3">
+            <div class="job-info">
+                <div class="job-profile">
+                    <h2>Modern Artist</h2>
+                    <img src="../Pictures/harjel.jpg" alt="">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+            <div class="salary">
+                <div>
+                    <p>25O$/hr</p>
+                    <p>Address</p>
+                </div>
+                <button>DETAILS</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="job-hiring">
+
+        <div class="job-cover4">
+            <div class="job-info">
+                <div class="job-profile">
+                    <h2>Pastel Artist</h2>
+                    <img src="../Pictures/tommy.jpg" alt="">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+            <div class="salary">
+                <div>
+                    <p>25O$/hr</p>
+                    <p>Address</p>
+                </div>
+                <button>DETAILS</button>
+            </div>
+        </div>
+
+        <div class="job-cover5">
+            <div class="job-info">
+                <div class="job-profile">
+                    <h2>Scenery Artist</h2>
+                    <img src="../Pictures/harjel.jpg" alt="">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+            <div class="salary">
+                <div>
+                    <p>25O$/hr</p>
+                    <p>Address</p>
+                </div>
+                <button>DETAILS</button>
+            </div>
+        </div>
+
+        <div class="job-cover6">
+            <div class="job-info">
+                <div class="job-profile">
+                    <h2>Designer</h2>
+                    <img src="../Pictures/jay.jpg" alt="">
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+            <div class="salary">
+                <div>
+                    <p>25O$/hr</p>
+                    <p>Address</p>
+                </div>
+                <button>DETAILS</button>
+            </div>
+        </div>
+    </div>
+</div>
